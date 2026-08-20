5ac932709ba9ec687e1ea9e4dc1f923f9e5f2a7a
Punsith-Rankalana <punsith.gamage@gmail.com>
2026-08-20 23:11:01 +0530

    Upate

--- Harbor.Api/Program.cs
+++ Harbor.Api/Program.cs
@@ -1,22 +1,35 @@
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddSingleton<Harbor.Api.Data.DbConnectionFactory>();
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Allow the React app (running on port 5173) to call this API
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app.UseHttpsRedirection();
+
+app.UseCors("AllowReactApp");   // <-- add this, before UseAuthorization
 
 app.UseAuthorization();
 

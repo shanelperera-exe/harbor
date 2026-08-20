cc5a06f33decd3e5bc45ff6ea1696c33790aeffd
Punsith-Rankalana <punsith.gamage@gmail.com>
2026-08-21 00:27:15 +0530

    Add Controllers and model For foundation

---
+++ Harbor.Api/Controllers/AuthController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Harbor.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        
+    }
+}
---
+++ harbor-frontend/src/services/api.js
@@ -0,0 +1,3 @@
+const API_BASE_URL = "http://localhost:5037/api";
+
+export default API_BASE_URL;
